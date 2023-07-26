928dccdf8dadf551b645e3c334a76c79ee1650cf
Hyojin Ju <hyojin.ju@gmx.net>
2023-07-26 22:42:59 +0900

    first practice

---
+++ app/reviews/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Heading from "@/components/Heading";
+
+export default function ReviewsPage() {
+    return (
+        <>
+            <Heading>Reviews</Heading>
+            <p>
+                Here we'll list all the reviews.
+            </p>
+            <ul>
+                <li>
+                    <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+                </li>
+                <li>
+                    <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+                </li>
+            </ul>
+        </>
+    )
+}
