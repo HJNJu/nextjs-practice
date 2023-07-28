61aac7722b9011346cf4af0ff695a06b1b2c70ed
Hyojin Ju <hyojin.ju@gmx.net>
2023-07-29 00:42:05 +0900

    style: add a link icon to ShareLinkButton

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LinkIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 
 export default function ShareLinkButton() {
@@ -10,13 +11,14 @@ export default function ShareLinkButton() {
         setClicked(true);
         setTimeout(() => setClicked(false), 1500);
     }
-    console.log('[ShareLinkButton] clicked:', clicked);
+
     return (
         <button onClick={handleClick}
-            className="border px-2 py-1 rounded text-slate-500 text-sm 
+            className="border flex gap-1 items-center px-2 py-1 rounded text-slate-500 text-sm 
             hover:bg-orange-100 hover:text-slate-700"
         >
-          {clicked ? "Link copied!" : "Share Link"}
+            <LinkIcon className="h-4 w-4" />
+            {clicked ? "Link copied!" : "Share Link"}
         </button>
     )
 }
