ab04b7b7297fc862c87c9e10679c785b60328d66
Hyojin Ju <hyojin.ju@gmx.net>
2023-08-02 01:19:33 +0900

    feat: pagination

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -4,16 +4,29 @@ import Link from "next/link";
 import Heading from "@/components/Heading";
 import { getReviews } from "@/lib/reviews";
 
+interface ReviewsPageProps {
+    searchParams: {
+        page?: string
+    };
+}
 
 export const metadata: Metadata = {
     title: "Reviews",
 };
 
-export default async function ReviewsPage() {
-    const reviews = await getReviews(6);
+const PAGE_SIZE = 6;
+
+export default async function ReviewsPage({ searchParams }: ReviewsPageProps) {
+    const page = parsePageParam(searchParams.page);
+    const reviews = await getReviews(PAGE_SIZE, page);
     return (
         <>
             <Heading>Reviews</Heading>
+            <div className="flex gap-2 pb-3">
+                <Link href={`/reviews?page=${page - 1}`}>&lt;</Link>
+                <span>Page {page}</span>
+                <Link href={`/reviews?page=${page + 1}`}>&gt;</Link>
+            </div>
             <ul className="flex flex-row flex-wrap gap-3">
                 {reviews.map((review, index) => 
                     <li key={review.slug} className="bg-white border rounded shadow w-80 hover:shadow-xl">
@@ -30,4 +43,14 @@ export default async function ReviewsPage() {
             </ul>
         </>
     )
+}
+
+function parsePageParam (paramValue: string): number {
+    if (paramValue) {
+        const page = parseInt(paramValue);
+        if (isFinite(page) && page > 0) {
+            return page;
+        }
+    }
+    return 1;
 }
